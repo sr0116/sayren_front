e2f6ad029503186e9680cbd30aa685d1dba3a0c3
sr0116 <kysy0116@gmail.com>
2025-09-18 14:34:28 +0900

    인덱스 페이지 리덕스 일부 적용

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import "./globals.css";
 import Header from "@/app/components/common/Header"
 import Footer from "@/app/components/common/Footer";
+import ReduxProvider from "@/app/providers/ReduxProvider";
 
 export const metadata = {
   title: "Sayren - 구독 결제 플랫폼",
@@ -11,9 +12,11 @@ export default function RootLayout({ children }) {
   return (
       <html lang="ko">
       <body className="flex flex-col min-h-screen font-sans">
-      <Header />
-      <main className="flex-grow pt-24">{children}</main>
-      <Footer />
+      <ReduxProvider>
+        <Header />
+        <main className="flex-grow pt-24">{children}</main>
+        <Footer />
+      </ReduxProvider>
       </body>
       </html>
   );
