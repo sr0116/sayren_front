985ddf7dceba667fdf68bc7a3e864a2134815ef4
manlubo <manlubo11@gmail.com>
2025-09-26 17:20:40 +0900

    비밀번호 찾기 / 재설정 추가

--- src/api/memberApi.js
+++ src/api/memberApi.js
@@ -3,3 +3,11 @@ import {useApiMutation} from "@/hooks/useApi";
 export function useFindIdMutation(options) {
   return useApiMutation("POST", "/api/user/member/find-email", { options });
 }
+
+export function useFindPwMutation(options) {
+  return useApiMutation("POST", "/api/user/member/find-pw", { options });
+}
+
+export function useResetPwMutation(options) {
+  return useApiMutation("POST", "/api/user/member/reset-pw", { options });
+}
