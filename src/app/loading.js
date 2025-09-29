1a160a8d3779395897093f20220e5028b8009936
sr0116 <kysy0116@gmail.com>
2025-09-29 11:13:25 +0900

    전역 설정에서 로딩 스피너 제거 후 마이페이지에서 따로 컴포넌트 설정

--- src/app/loading.js
+++ src/app/loading.js
@@ -1,5 +1,6 @@
 import LoadingSpinner from "@/components/common/loading/LoadingSpinner";
+import GlobalLoader from "@/components/common/loading/GlobalLoader";
 
 export default function Loading() {
-  return <LoadingSpinner size="lg" fullscreen />;
+  return <GlobalLoader size="lg" fullscreen />;
 }
