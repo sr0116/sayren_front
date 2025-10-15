1228293a92e0de086950f88b8a6302fa31b0fe80
sr0116 <kysy0116@gmail.com>
2025-10-15 09:48:44 +0900

    어드민 페이지 쪽에도 로딩 스피너 추가

--- src/app/admin/layout.js
+++ src/app/admin/layout.js
@@ -1,4 +1,5 @@
 import AdminMenu from "@/components/admin/AdminMenu";
+import SectionLoader from "@/components/common/loading/SectionLoader";
 
 export default function AdminRayOut({children}) {
   return(
@@ -11,7 +12,9 @@ export default function AdminRayOut({children}) {
 
       {/* 오른쪽 컨텐츠 영역 */}
       <section className="flex-grow border border-gray-200 rounded-lg p-6 bg-white shadow-sm">
-        {children}
+        <SectionLoader padded={false} color="gray-900">
+          {children}
+        </SectionLoader>
       </section>
     </div>
   )
